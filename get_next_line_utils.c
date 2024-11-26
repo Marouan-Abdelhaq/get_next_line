77ae0fd3b55a9432b84e9b7cafda0f0c9758df9d
Marouan-Abdelhaq <abdelhaqmarouan70@gmail.com>
2024-11-26 09:55:09 +0100

    mod

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: mabdelha <mabdelha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 09:06:02 by mabdelha          #+#    #+#             */
-/*   Updated: 2024/11/25 18:44:28 by mabdelha         ###   ########.fr       */
+/*   Updated: 2024/11/25 21:20:07 by mabdelha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,35 +22,6 @@ size_t	ft_strlen(const char *str)
 		i++;
 	}
 	return (i);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	char	*str;
-	int		len_s1;
-	int		len_s2;
-	int		i;
-	int		j;
-
-	len_s1 = ft_strlen(s1);
-	len_s2 = ft_strlen(s2);
-	i = 0;
-	j = 0;
-	str = (char *)malloc(sizeof(char) * (len_s1 + len_s2 + 1));
-	if (!str)
-		return (NULL);
-	while (i < len_s1)
-	{
-		str[i] = s1[i];
-		i++;
-	}
-	while (j < len_s2)
-	{
-		str[i + j] = s2[j];
-		j++;
-	}
-	str[i + j] = '\0';
-	return (str);
 }
 
 int	ft_read_from_fd(int fd, char *tab)
